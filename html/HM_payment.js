1a94b8a25f0d27557bc168bd345ffc861ce0521b
thissarthak <sarthakkathpalia@gmail.com>
2021-11-03 20:34:39 +0530

    HM booking added

---
+++ html/HM_payment.js
@@ -0,0 +1,41 @@
+
+function paymentProcess() {
+
+
+    console.log("Ji");
+    var options = {
+        "key": "rzp_test_glRffi9JjZvHkL", // Enter the Key ID generated from the Dashboard
+        "amount": 15000000*100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 means 50000 paise or ₹500.
+        "currency": "INR",
+        "name": "HONEYMOON ON MOON",
+        "description": "SPECIAL PACKAGE",
+        "image": "payment_LEO.jpeg",// Replace this with the order_id created using Orders API (https://razorpay.com/docs/api/orders).
+        "handler": function (response){
+            savetoDB(response);
+            $('#myModal').modal();
+        },
+        "prefill": {
+            "name": "Shyam Kumar",
+            "email": "shyamkumar@gmail.com",
+            "contact": "9999999999"
+        },
+        "notes": {
+            "address": "note value"
+        },
+        "theme": {
+            "color": "#9932CC"
+        }
+    }
+    var propay = new Razorpay(options);
+    propay.open();
+}
+
+
+function savetoDB(response) {
+    console.log(response)
+    var payRef = firebase.database().ref('payment');
+
+    payRef.child('123456789').set({
+    payment_id : response.razorpay_payment_id
+    })
+}
